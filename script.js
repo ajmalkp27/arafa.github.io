b7167c6ed69dbe9f07e5c98bbeb79aefb8077357
AJMAL KP <63350930+ajmalkp27@users.noreply.github.com>
2022-07-06 16:47:31 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,17 +3,12 @@ const navMenu = document.getElementById('nav-menu');
 const header = document.getElementById('header');
 
 document.onclick = function(e){
-    if (e.target.id !== 'hamburger' ){
+    if (e.target.id !== 'hamburger' && e.target.id !== 'nav-menu' ){
         hamburger.classList.remove('active');
         navMenu.classList.remove('active');
         
     }
 }
-
-
-
-
-
 
 
 hamburger.addEventListener("click",()=>{
@@ -24,3 +19,32 @@ hamburger.addEventListener("click",()=>{
 
 })
 
+
+let text=document.getElementById('text');
+window.addEventListener('scroll',function(){
+    let value=window.scrollY;
+   
+    text.style.right = value * 4 + 'px';
+    
+})
+
+
+function slides() { 
+    var myIndex = 0;
+    carousel();
+    function carousel(){
+        var i;
+        var x= document.getElementsByClassName("myslides");
+        for(i=0;i<x.length;i++) {
+            x[i].style.display= "none";
+        }
+        myIndex++;
+        if (myIndex > x.length){ myIndex=1}
+        x[myIndex-1].style.display="block";
+        setTimeout(carousel,2000);
+
+
+    }
+
+ }
+ slides()
