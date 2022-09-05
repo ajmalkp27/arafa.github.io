99871a816ad9ea095d3e19fc3e1a2cf03c8782ea
AJMAL KP <63350930+ajmalkp27@users.noreply.github.com>
2022-09-05 22:18:09 +0530

    last comit

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ const header = document.getElementById('header');
 
 const toggle = document.getElementById('toggle');
 const navbar = document.getElementById('navbar');
+
+const id=document.getElementById('imgBx');
+
 document.onclick = function(e){
     if (e.target.id !== 'toggle' && e.target.id !== 'toggle'){
         toggle.classList.remove('active');
@@ -69,3 +72,26 @@ function slidesi() {
  slidesp()
 
  document.getElementById("myForm").reset();
+
+
+
+ function slideid() { 
+    var myIndex = 0;
+    carousel();
+    function carousel(){
+        var i;
+        var x= document.getElementsByClassName("imgBx");
+        for(i=0;i<x.length;i++) {
+            x[i].style.display= "none";
+        }
+        myIndex++;
+        if (myIndex > x.length){ myIndex=1}
+        x[myIndex-1].style.display="block";
+
+        setTimeout(carousel,2000);
+
+
+    }
+
+ }
+ slideid()
